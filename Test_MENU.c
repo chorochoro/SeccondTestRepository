c361eb65434b19705ed4e45b8b10ff9a9324e395
ChoroOP <elementaloffire@yahoo.co.jp>
2014-02-08 11:15:30 +0900

    Create Test_MENU.c

---
+++ Test_MENU.c
@@ -0,0 +1,12 @@
+int main()
+{
+  printf("Hello Choro Repository\n");
+  while(1)
+  {
+    printf("###############\n");
+    printf("# MENU        #|\n");
+    printf("###############|\n");
+    printf(" |||||||||||||||\n");
+  }
+  return 0;
+}
